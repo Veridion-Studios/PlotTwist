16faa41b3363694c8ed69d83d0f3d46716e32a9a
TheAwesomeAJ <theawesomeaj.dev@gmail.com>
2025-09-12 15:37:02 -0400

    Created app using Create-Next-App. Added ShadCN components from ui.shadcn.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,1 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <nav>
-        <div>Centered</div>
-      </nav>
-    </div>
-  );
-}
+import { Button } from "@/components/ui/button"
